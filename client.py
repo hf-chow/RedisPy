be28b70baf80f51a97493cb2e6139995b3439885
Hon Fung CHOW <honfung.chow@sia-partners.com>
2023-05-10 14:47:16 +0800

    add server skeleton

--- client.py
+++ client.py
@@ -7,4 +7,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
     s.send(b"*1\r\n$4\r\nping\r\n")
     data = s.recv(1024)
+    print("Sent PING")
 
--- server.py
+++ server.py
@@ -1,18 +1,45 @@
-import socket
+import gevent
+from gevent.server import StreamServer
+from gevent.pool import Pool
 
 # Redis uses port 6379; we need to bind port 6379 to a TCP Server
 HOST = "localhost"
 PORT = 6379
 
-# Server respond to ping with pong
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
+class ProtocolHandler():
+    def handle_request(self):
+        pass
+
+    def write_response(self):
+        pass
+
+class Server():
+    def __init__(self, host, port, max_clients):
+        self.pool = Pool(max_clients)
+        self.server = StreamServer((host, port), 
+                                   self.connection_handler,
+                                   spawn=self.pool)
+        self.protocol = ProtocolHandler()
+        self.kv = {}
+
+    def connection_handler(self, conn, addr):
+        sf = conn.makefile('rwb')
+
         while True:
-            data = conn.recv(1024)
-            if data = b"*1\r\n$4\r\nping\r\n": #Ping looks monstrous due to RESP
-                conn.send(b"+PONG\r\n")
+            try:
+                data = self.protocol.handle_request(sf)
+            except:
                 break
+            
+            try:
+                resp = self.get_response(data)
+            except:
+                break
+
+            self.protocol.write_response(sf, resp)
+
+    def get_response(self, data):
+        pass
+
+    def run(self):
+        self.server.serve_forever()
